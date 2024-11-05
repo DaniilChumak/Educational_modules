4af5bd64ab2f285592b47160bb1bc41017561cb9
user <danilchumak80@gmail.com>
2024-11-05 21:28:43 +0500

    Подправили оформление

--- modules/urls.py
+++ modules/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 
 from modules.apps import ModulesConfig
 from modules.views import (ModuleViewSet, LessonCreateAPIView,
-                             LessonDestroyAPIView, LessonListAPIView,
-                             LessonRetrieveAPIView, LessonUpdateAPIView)
+                           LessonDestroyAPIView, LessonListAPIView,
+                           LessonRetrieveAPIView, LessonUpdateAPIView)
 
 app_name = ModulesConfig.name
 
@@ -12,12 +12,8 @@ urlpatterns = [
     path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
     path("lessons/", LessonListAPIView.as_view(), name="lesson_list"),
     path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
-    path(
-        "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
-    ),
-    path(
-        "lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"
-    ),
+    path("lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
+    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
 ]
 
 router = routers.SimpleRouter()
