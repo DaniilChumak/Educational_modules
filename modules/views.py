9af29119cf6a0d7614120c58aaf6c802cbd544b3
user <danilchumak80@gmail.com>
2024-11-05 21:08:58 +0500

    Добавили импорт из serializers.py

--- modules/views.py
+++ modules/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from modules.models import Module, Lesson
+from modules.serializers import ModuleSerializer, LessonSerializer
+
+"""Эндпоинт для модели образовательного модуля"""
+class ModuleViewSet(ModelViewSet):
+
+    serializer_class = ModuleSerializer
+    queryset = Module.objects.all()
+
+
+"""Эндпоинт для создания урока"""
+class LessonCreateAPIView(CreateAPIView):
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+"""Эндпоинт для изменения урока"""
+class LessonUpdateAPIView(UpdateAPIView):
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+"""Эндпоинт для вывода списка всех уроков"""
+class LessonListAPIView(ListAPIView):
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+"""Эндпоинт для просмотра одного урока"""
+class LessonRetrieveAPIView(RetrieveAPIView):
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+"""Эндпоинт для удаления урока"""
+class LessonDestroyAPIView(DestroyAPIView):
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
