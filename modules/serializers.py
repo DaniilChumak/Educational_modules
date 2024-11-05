771af4dabadf3dcd472e72ef4f6b0d2ae06a6ce9
user <danilchumak80@gmail.com>
2024-11-05 21:06:46 +0500

    Создали сериализатор для моделей Модуль и Урок

---
+++ modules/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from modules.models import Module, Lesson
+
+"""Сериализатор для модели образовательного модуля"""
+class EducationModuleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Module
+        fields = "__all__"
+
+
+"""Сериализатор для модели урока"""
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
