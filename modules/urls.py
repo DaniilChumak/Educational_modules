12e775363e3fb49557bc871cd20ab0ae29557372
user <danilchumak80@gmail.com>
2024-11-05 21:20:42 +0500

    Добавили пути для Урока и Модуля

---
+++ modules/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from rest_framework import routers
+
+from modules.apps import ModulesConfig
+from modules.views import (ModuleViewSet, LessonCreateAPIView,
+                             LessonDestroyAPIView, LessonListAPIView,
+                             LessonRetrieveAPIView, LessonUpdateAPIView)
+
+app_name = ModulesConfig.name
+
+urlpatterns = [
+    path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lessons/", LessonListAPIView.as_view(), name="lesson_list"),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
+    path(
+        "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
+    ),
+    path(
+        "lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"
+    ),
+]
+
+router = routers.SimpleRouter()
+router.register("modules", ModuleViewSet)
+
+urlpatterns += router.urls
