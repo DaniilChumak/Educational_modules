a304130907ca5dad6b19dcfd239bd3e93b5004db
user <danilchumak80@gmail.com>
2024-11-05 20:58:41 +0500

    Создали приложение пользователей Создали модель пользователя и применили миграции

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Email")
+    first_name = models.CharField(max_length=150, verbose_name="Имя", **NULLABLE)
+    last_name = models.CharField(max_length=200, verbose_name="Фамилия", **NULLABLE)
+    country = models.CharField(max_length=150, verbose_name="Страна", **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/", verbose_name="Аватар", **NULLABLE)
+    last_login = models.DateTimeField(**NULLABLE, verbose_name="Дата последнего входа")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("email",)
