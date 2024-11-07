ad3f3ca1ed76111ac01fa0587fb1be035f2622e0
user <danilchumak80@gmail.com>
2024-11-07 15:33:28 +0500

    Добавили модели для отображения в панели администратора

--- modules/admin.py
+++ modules/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from modules.models import Module, Lesson
 
-# Register your models here.
+
+"""Добавили модель Module для отображения в панели администратора"""
+@admin.register(Module)
+class EducationModuleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description",)
+    list_filter = ("title",)
+    search_fields = ("title",)
+
+
+"""Добавили модель Lesson для отображения в панели администратора"""
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("number", "title", "module",)
+    list_filter = ("number",)
+    search_fields = ("title", "module",)
