c6f72bc40ab94f85094a5e6502b5b4be28e87208
user <danilchumak80@gmail.com>
2024-11-07 23:32:41 +0500

    Написали эндпоинт для модели User

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("modules/", include("modules.urls", namespace="modules")),
+    path("users/", include("users.urls", namespace="users")),
 ]
