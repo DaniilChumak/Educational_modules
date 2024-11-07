76060315490881c7c8cf5e1ddd2533e53f0acfec
user <danilchumak80@gmail.com>
2024-11-07 16:34:21 +0500

    Добавили вложенность уроков в модуль

--- modules/serializers.py
+++ modules/serializers.py
@@ -2,17 +2,22 @@ from rest_framework import serializers
 
 from modules.models import Module, Lesson
 
-"""Сериализатор для модели образовательного модуля"""
-class ModuleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Module
-        fields = "__all__"
-
-
 """Сериализатор для модели урока"""
 class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+"""Сериализатор для модели образовательного модуля"""
+class ModuleSerializer(serializers.ModelSerializer):
+    """Вложенность списка уроков в модуль"""
+    lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
+
+    class Meta:
+        model = Module
+        fields = "__all__"
+
+
+
