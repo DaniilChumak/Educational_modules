8237fa2809f5010c0dba888977b50630d0edf697
user <danilchumak80@gmail.com>
2024-11-07 23:30:10 +0500

    Оформление

--- users/models.py
+++ users/models.py
@@ -7,13 +7,39 @@ NULLABLE = {"blank": True, "null": True}
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(unique=True, verbose_name="Email")
-    first_name = models.CharField(max_length=150, verbose_name="Имя", **NULLABLE)
-    last_name = models.CharField(max_length=200, verbose_name="Фамилия", **NULLABLE)
-    country = models.CharField(max_length=150, verbose_name="Страна", **NULLABLE)
-    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
-    avatar = models.ImageField(upload_to="users/", verbose_name="Аватар", **NULLABLE)
-    last_login = models.DateTimeField(**NULLABLE, verbose_name="Дата последнего входа")
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email"
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name="Имя",
+        **NULLABLE
+    )
+    last_name = models.CharField(
+        max_length=200,
+        verbose_name="Фамилия",
+        **NULLABLE
+    )
+    country = models.CharField(
+        max_length=150,
+        verbose_name="Страна",
+        **NULLABLE
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/",
+        verbose_name="Аватар",
+        **NULLABLE
+    )
+    last_login = models.DateTimeField(
+        verbose_name="Дата последнего входа",
+        **NULLABLE
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
